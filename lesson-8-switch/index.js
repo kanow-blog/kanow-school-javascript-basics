7b5a1d8ec66017fdca5464685e6671d97adf4517
yurukyuruk <yurukyuruk@gmail.com>
2021-03-31 22:26:15 +0300

    lesson-8 is completed

--- lesson-8-switch/index.js
+++ lesson-8-switch/index.js
@@ -1,2 +1,28 @@
 import readmeLoader from '../readmeContentLoader.js';
 void readmeLoader();
+
+//Task-1
+
+function printAnimalType(animalType) {
+    switch (animalType) {
+        case 'tiger':
+            console.log('Animal ' + animalType + ' is a cat');
+            break;
+        case 'husky':
+            console.log('Animal ' + animalType + ' is a dog');
+            break;
+        case 'spider':
+            console.log('Animal ' + animalType + ' is an invertebrate');
+            break;
+        case 'shark':
+            console.log('Animal ' + animalType + ' is a fish');
+            break;
+        default:
+            console.log(animalType + ' is an unexpected animal');
+    }
+}
+printAnimalType('tiger');
+printAnimalType('husky');
+printAnimalType('spider');
+printAnimalType('shark');
+printAnimalType('bee');
