c3a5e4ad1edcb6568c152cbc38aa691aab8e453e
yurukyuruk <yurukyuruk@gmail.com>
2021-04-10 03:16:03 +0300

    lesson 6 updated

--- lesson-5-array/index.js
+++ lesson-5-array/index.js
@@ -3,23 +3,57 @@ void readmeLoader();
 
 //TASK-1
 
+let numbersArray = [1, 2, 3, 4, 5];
+console.log('TASK-1', numbersArray);
+
 //TASK-2
+
+console.log('TASK-2', numbersArray[2]);
 
 //TASK-3
 
+numbersArray.push(10);
+console.log('TASK-3', numbersArray);
+
 //TASK-4
+
+numbersArray.unshift(-1);
+console.log('TASK-4', numbersArray);
 
 //TASK-5
 
+numbersArray.pop();
+console.log('TASK-5', numbersArray);
+
 //TASK-6
+
+numbersArray.splice(2, 1);
+console.log('TASK-6', numbersArray);
 
 //TASK-7
 
+numbersArray[4];
+console.log('TASK-7', numbersArray[4]);
+
 //TASK-8
+
+function createOnesMatrix(rownsCount, columnsCount) {
+    let matrix = [];
+    for(let i = 0; i < rownsCount; i++) {
+        for() {
+
+        }
+        matrix.push();
+    }
+    let var1 = [1, 2, 3]; 
+
+    let var2 = [[1, 1], [1, 1]];
+    var2[0][0]
+}
+console.log('TASK-8', createOnesMatrix(1, 1) {matrix});
 
 //TASK-9
 
-
-console.log('QUESTION-1: ', 'PUT YOUR ANSWER HERE');
-console.log('QUESTION-2: ', 'PUT YOUR ANSWER HERE');
-console.log('QUESTION-3: ', 'PUT YOUR ANSWER HERE');
+console.log('QUESTION-1: ', 'Console will print undefined.');
+console.log('QUESTION-2: ', '');
+console.log('QUESTION-3: ', '');
--- lesson-6-object/index.js
+++ lesson-6-object/index.js
@@ -3,23 +3,73 @@ void readmeLoader();
 
 //TASK-1
 
+let person = {
+    firstName: 'Ali',
+    lastName: 'Kenzo',
+    age: '23',
+    hello: function() {
+        console.log('Hello my name is: ' + this.firstName + ' ' + this.lastName);
+    }
+}
+console.log('TASK-1', person.hello());
+
 //TASK-2
+
+person.familyMembers = ['Veli', 'Hasan'];
+console.log('TASK-2', person);
 
 //TASK-3
 
+person.house = {
+    country: 'Nepal',
+    city: 'Kathmandu',
+    zipCode: '36950'
+}
+console.log('TASK-3', person);
+
 //TASK-4
+/*
+person.addField = function(name, value) {
+    this[name] = value;
+}
+addField[weight, '60kg');
+console.log('TASK-4', person);
 
 //TASK-5
+do it like task-4
+function addFamilyMember(newPerson) {
+    person.familyMembers.push(newPerson);
+}
+addFamilyMember(Kerime);
+console.log('TASK-5', person);
 
 //TASK-6
 
+function addField2(name, value) {
+    person.name = value;
+}
+addField2(eyeColor, 'green');
+console.log('TASK-6', person);
+
 //TASK-7
 
+person.familyMembers[Kerime] = {
+    firstName: 'Kerime',
+    lastName: 'Kerr',
+    age: 43
+}
+*/
 //TASK-8
+
+person.house.country = 'Void';
+console.log('TASK-8', person);
 
 //TASK-9
 
-console.log('QUESTION-1: ', 'PUT YOUR ANSWER HERE');
-console.log('QUESTION-2: ', 'PUT YOUR ANSWER HERE');
-console.log('QUESTION-3: ', 'PUT YOUR ANSWER HERE');
-console.log('QUESTION-4: ', 'PUT YOUR ANSWER HERE');
+delete person.house.zipCode;
+console.log('TASK-9', person);
+
+console.log('QUESTION-1: ', 'We  can use object freeze method.');
+console.log('QUESTION-2: ', 'We can use object seal method.');
+console.log('QUESTION-3: ', 'We can use object.values(variable); for it.');
+console.log('QUESTION-4: ', 'We can use object.keys(variable); fot it.');
