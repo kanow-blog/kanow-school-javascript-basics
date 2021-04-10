5e19681f02cb89d525229e3c8ed62299c279ee43
yurukyuruk <yurukyuruk@gmail.com>
2021-04-10 03:26:13 +0300

    lesson 11 updated

--- lesson-11-loops/index.js
+++ lesson-11-loops/index.js
@@ -1,16 +1,64 @@
 import readmeLoader from '../readmeContentLoader.js';
 void readmeLoader();
 
-//TASK-1
+//Task-1
 
-//TASK-3
+var person = {
+    firstName: 'Ali',
+    lastName: 'Webbe',
+    age: '32'
+};
+for(const property in person) {
+    console.log('TASK-1', property);
+}
 
-//TASK-4
+//Task-2
 
-//TASK-5
+const myArray = ['x', 'y', 'z'];
+for(const element of myArray) {
+    console.log('TASK-2', element);
+}
+for(let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+}
 
-//TASK-6
+//Task-3
+function checkIsTheMiddle(array, index) {
+    return Math.floor((array.length - 1) / 2) === index;
+}
+const fiveNumbersOfArray = [11, 12, 13, 14, 15];
+const fourElementsArray = [1, 2, 3, 4];
+let i = 0;
+while (checkIsTheMiddle(fiveNumbersOfArray, i) === false) {
+    console.log('TASK-3', fiveNumbersOfArray[i]);
+    i++; 
+}
+console.log('TASK-3', fiveNumbersOfArray[i]);
+i = 0;
+while (checkIsTheMiddle(fourElementsArray, i) === false) {
+    console.log('TASK-3', fourElementsArray[i]);
+    i++;
+}
+console.log('TASK-3', fourElementsArray[i]);
 
-console.log('QUESTION-1: ', 'PUT YOUR ANSWER HERE');
-console.log('QUESTION-2: ', 'PUT YOUR ANSWER HERE');
-console.log('QUESTION-3: ', 'PUT YOUR ANSWER HERE');
+
+//Task-4
+
+const array1 = [1, 2, 3, 4, 5, 6]; 
+for(let j = 0; j < array1.length; j++) {
+    if(j % 2 === 0) {
+        continue;
+    }
+    console.log('TASK-4', array1[j]);  
+}
+
+
+//Task-5
+
+const array3 = [1, 2, 3, 4, 5, 6];
+for(const element of array3) {
+    if(element === 4) {
+        break;
+    }
+    console.log('TASK-5', element);
+}
