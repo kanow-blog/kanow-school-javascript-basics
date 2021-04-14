9ce521ca85636a8ea1cd3349f641826ee322f506
yurukyuruk <yurukyuruk@gmail.com>
2021-04-15 01:02:02 +0300

    changes in lesson 1

--- lesson-1-variables-basics/index.js
+++ lesson-1-variables-basics/index.js
@@ -10,3 +10,7 @@ console.log('TASK-1', firstName);
 
 var age = 25;
 console.log('TASK-2', age);
+
+//Question
+
+console.log('QUESTION-1', 'Variables are containers that store data inside. For example: let var1 = 12; ==> var1 variable stores number 12 inside.')
