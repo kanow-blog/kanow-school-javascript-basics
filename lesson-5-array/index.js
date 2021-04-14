19335c3ca43e69d326b15c3063998ca285bb3d67
yurukyuruk <yurukyuruk@gmail.com>
2021-04-15 00:50:49 +0300

    changes in lesson 5

--- lesson-5-array/index.js
+++ lesson-5-array/index.js
@@ -50,12 +50,18 @@ function createOnesMatrix(rowsCount, columnsCount) {
 let thing = createOnesMatrix(4, 4);
 console.log('TASK-8', thing);
 
-//TASK-9
-for(let i = 0; i < 4; i++) {
-    console.log('TASK-9', thing[i][0]);
+for(let i = 0; i < thing.length; i++) {
+    console.log('TASK-8', thing[i][0]);
 }
-console.log('TASK-9', thing[0]);
+console.log('TASK-8', thing[0]);
+
+//Task-9 Create a loop to print eack matrix elements 
+for(let i = 0; i < thing.length; i++) {
+    for(let j = 0; j < thing.length; j++) {
+        console.log('Value under row: ['+ i +'] and column: ['+ j +']', thing[i][j]);
+    }  
+}
 
 console.log('QUESTION-1: ', 'Console will print undefined.');
-console.log('QUESTION-2: ', 'Yes we can.');
-console.log('QUESTION-3: ', 'We can hold many values under a single name. let color1 = ''blue''; let color2 = ''green''; let color3 = ''orange''; And we can get them all in one line==> let colors = [''blue'', ''green'', ''orange''].');
+console.log('QUESTION-2: ', 'Yes we can. If we increase the lenght of the array, empty elements are going to be added to that array. If we decrease the length, it will be deleting elements from the end till it reaches to the lenght we set.');
+console.log('QUESTION-3: ', 'We can hold many values under a single variable. let color1 = "blue"; let color2 = "green"; let color3 = "orange"; And we can get them all in one line==> let colors = ["blue", "green", "orange"].');
