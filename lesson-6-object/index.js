21be9f4f8556f3613363f90194d9dacbf1b19996
yurukyuruk <64555726+yurukyuruk@users.noreply.github.com>
2021-04-16 01:26:31 +0300

    lesson 6 updated (#12)
    * lesson 6 updated
    
    * removing some data
    
    * new additions to lesson 6
    
    * lesson 6 updated
    
    Co-authored-by: kaczor6418 <k.kaczynski97@gmail.com>

--- lesson-6-object/index.js
+++ lesson-6-object/index.js
@@ -3,23 +3,71 @@ void readmeLoader();
 
 //TASK-1
 
+let person = {
+    firstName: 'Ali',
+    lastName: 'Kenzo',
+    age: '23',
+    hello: function() {
+        console.log('Hello my name is: ' + this.firstName + ' ' + this.lastName);
+    }  
+}
+console.log('TASK-1', person);
+
 //TASK-2
+
+person.familyMembers = ['Veli', 'Hasan'];
+console.log('TASK-2', person);
 
 //TASK-3
 
+person.house = {
+    country: 'Nepal',
+    city: 'Kathmandu',
+    zipCode: '36950'
+}
+console.log('TASK-3', person);
+
 //TASK-4
+
+person.addField = function(name, value) {
+    this[name] = value;
+}
+console.log('TASK-4', person);
 
 //TASK-5
 
+person.addFamilyMember = function(newPerson) {
+        this.familyMembers.push(newPerson);
+}
+person.addFamilyMember('Kerime');
+console.log('TASK-5', person);
+
 //TASK-6
+
+person.addField('eyeColor', 'green');
+console.log('TASK-6', person);
 
 //TASK-7
 
+person.addFamilyMember('Burak');
+person.familyMembers[3] = {
+    firstName: 'Burak',
+    lastName: 'Kenan',
+    age: 43
+}
+console.log('TASK-7', person);
+
 //TASK-8
+
+person.house.country = 'Void';
+console.log('TASK-8', person);
 
 //TASK-9
 
-console.log('QUESTION-1: ', 'PUT YOUR ANSWER HERE');
-console.log('QUESTION-2: ', 'PUT YOUR ANSWER HERE');
-console.log('QUESTION-3: ', 'PUT YOUR ANSWER HERE');
-console.log('QUESTION-4: ', 'PUT YOUR ANSWER HERE');
+delete person.house.zipCode;
+console.log('TASK-9', person);
+
+console.log('QUESTION-1: ', 'We  can use object freeze method.');
+console.log('QUESTION-2: ', 'We can use object seal method.');
+console.log('QUESTION-3: ', 'We can use object.values(variable); for it.');
+console.log('QUESTION-4: ', 'We can use object.keys(variable); fot it.');
