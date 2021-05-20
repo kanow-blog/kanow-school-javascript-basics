af417213f5f0d264bd0bd52937514bbf28446ad7
yurukyuruk <yurukyuruk@gmail.com>
2021-05-20 23:10:16 +0300

    lesson-13-completed

--- lesson-13-array-methods/index.js
+++ lesson-13-array-methods/index.js
@@ -20,40 +20,55 @@ const nestedArrayOfNumbers = [
 ];
 
 //TASK-1
-console.log('TASK-1');
+const evenNumbers = arrayOfNumbers.filter(number => number % 2 === 0);
+console.log('TASK-1', evenNumbers);
 
 //TASK-2
-console.log('TASK-2');
+const oddNumbers = arrayOfNumbers.filter(number => number % 2 !== 0);
+console.log('TASK-2', oddNumbers);
 
 //TASK-3
-console.log('TASK-3');
+const maxNumber = Math.max(...arrayOfNumbers);
+console.log('TASK-3', maxNumber);
 
 //TASK-4
-console.log('TASK-4');
+arrayOfNumbers.sort((a, b) => a - b);
+console.log('TASK-4', arrayOfNumbers);
 
 //TASK-5
-console.log('TASK-5');
+arrayOfNumbers.sort((a, b) => b - a);
+console.log('TASK-5', arrayOfNumbers);
 
 //TASK-6
-console.log('TASK-6');
+const isEvenNumbers = (number) => number % 2 === 0;
+console.log('TASK-6', arrayOfNumbers.every(isEvenNumbers));
 
 //TASK-7
-console.log('TASK-7');
+const isOddNumber = (number) => number % 2 !== 0;
+console.log('TASK-7', arrayOfNumbers.some(isOddNumber));
 
 //TASK-8
-console.log('TASK-8');
+const arrayOfPersonsFullName = arrayOfPersons.map(x => x.firstName + ' ' + x.lastName);
+console.log('TASK-8', arrayOfPersonsFullName);
 
 //TASK-9
-console.log('TASK-9');
+const findLastName = arrayOfPersons.find(element => element.lastName === 'Nowak');
+console.log('TASK-9', findLastName);
 
 //TASK-10
-console.log('TASK-10');
+const findIndexOfPerson = (element) => element.lastName === 'Nowak';
+console.log('TASK-10', arrayOfPersons.findIndex(findIndexOfPerson));
 
 //TASK-11
-console.log('TASK-11');
+const multipliedBy3 = evenNumbers.map(x => x * 3);
+console.log('TASK-11', multipliedBy3);
 
 //TASK-12
-console.log('TASK-12');
+const isEvenSubarray = (number) => number % 2 === 0;
+const evenSubarrays = nestedArrayOfNumbers.filter(subarray => subarray.every(isEvenSubarray)).flat().map(x => x * 2);
+console.log('TASK-12', evenSubarrays);
 
 //TASK-13
-console.log('TASK-13');
+const IsOddNumber = (number) => number % 2 !== 0;
+const subarraysWithOddNumber = nestedArrayOfNumbers.filter(subarray => subarray.some(isOddNumber)).flat().map(x => x * 2);
+console.log('TASK-13', subarraysWithOddNumber);
